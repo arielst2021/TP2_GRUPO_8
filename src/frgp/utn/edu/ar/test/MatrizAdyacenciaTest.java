5a605b5d623d758314df8e9d84a257a510fe4870
ArielTapia <arieltapia42@gmail.com>
2022-04-10 01:07:50 -0300

    Puntos_1_2_3a_y3b
    Métodos agregarElementoTest y agregarElementoSimetriaTest

---
+++ src/frgp/utn/edu/ar/test/MatrizAdyacenciaTest.java
@@ -0,0 +1,23 @@
+package frgp.utn.edu.ar.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import frgp.utn.edu.ar.main.MatrizAdyacencia;
+
+
+public class MatrizAdyacenciaTest {
+	
+	MatrizAdyacencia mAdyacencia = new MatrizAdyacencia(4);
+	
+	@Test
+	public void agregarElementoTest() {
+		mAdyacencia.agregarElemento(0, 3);
+		assertTrue(mAdyacencia.existeElemento(3,0));
+	}
+	
+	@Test
+	public void agregarElementoSimetriaTest() {
+		mAdyacencia.agregarElemento(0, 3);
+		assertTrue(mAdyacencia.existeElemento(3,0));
+	}
+}
