b5a955f25cda43fb8f1e7d64d36eadfadc292c42
dadistefano <diegods_91@hotmail.com>
2022-04-14 00:43:18 -0300

    Test puntos c)d)e)f)

--- src/frgp/utn/edu/ar/test/MatrizAdyacenciaTest.java
+++ src/frgp/utn/edu/ar/test/MatrizAdyacenciaTest.java
@@ -1,5 +1,7 @@
 package frgp.utn.edu.ar.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import frgp.utn.edu.ar.main.MatrizAdyacencia;
@@ -20,4 +22,47 @@ public class MatrizAdyacenciaTest {
 		mAdyacencia.agregarElemento(3, 0);
 		assertTrue(mAdyacencia.existeElemento(0,3));
 	}
+	
+	@Test
+	public void eliminarElementoTest() {
+		mAdyacencia.eliminarElemento(3, 0);
+		assertFalse(mAdyacencia.existeElemento(3,0)); //Existe elemento devuelve un booleano si no existe espero que sea falso
+	}
+	
+	@Test
+	public void eliminarElementoSimetricoTest() {
+		mAdyacencia.eliminarElemento(0, 3);
+		assertFalse(mAdyacencia.existeElemento(0,3));
+	}
+
+	@Test
+	public void contarRelacionesTest() {
+		mAdyacencia.agregarElemento(0, 3);
+		mAdyacencia.agregarElemento(1, 2);
+		mAdyacencia.agregarElemento(2, 3);
+		assertEquals(3, mAdyacencia.getCantidadElementos());
+	}
+	
+	@Test
+	public void existenTodosLosElementosTest() {
+		mAdyacencia.agregarElemento(0, 1);
+		mAdyacencia.agregarElemento(0, 2);
+		mAdyacencia.agregarElemento(0, 3);
+		mAdyacencia.agregarElemento(1, 2);
+		mAdyacencia.agregarElemento(1, 3);
+		mAdyacencia.agregarElemento(2, 3);
+		
+		if (mAdyacencia.getCantidadElementos()==6)
+		{
+			assertFalse(mAdyacencia.existeElemento(0,0));
+			assertFalse(mAdyacencia.existeElemento(1,1));
+			assertFalse(mAdyacencia.existeElemento(2,2));
+			assertFalse(mAdyacencia.existeElemento(3,3));
+		}
+	}
+	
+	
+	
+	
+	
 }
