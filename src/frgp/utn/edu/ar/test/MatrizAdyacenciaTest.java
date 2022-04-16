531504fc7175ca7419c1bfb0c51fb928dcc846fe
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2022-04-15 22:10:16 -0300

    cambio punto c y d
    use assertAll() para agrupar todos los assertFalse()

--- src/frgp/utn/edu/ar/test/MatrizAdyacenciaTest.java
+++ src/frgp/utn/edu/ar/test/MatrizAdyacenciaTest.java
@@ -2,7 +2,11 @@ package frgp.utn.edu.ar.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import frgp.utn.edu.ar.main.MatrizAdyacencia;
 
@@ -25,14 +29,47 @@ public class MatrizAdyacenciaTest {
 	
 	@Test
 	public void eliminarElementoTest() {
-		mAdyacencia.eliminarElemento(3, 0);
-		assertFalse(mAdyacencia.existeElemento(3,0)); //Existe elemento devuelve un booleano si no existe espero que sea falso
+		// Agregamos los elementos que no tengan simetricos para probar el metodo
+		mAdyacencia.agregarElemento(0, 0);
+		mAdyacencia.agregarElemento(0, 1);
+		mAdyacencia.agregarElemento(0, 2);
+		mAdyacencia.agregarElemento(1, 1);
+		mAdyacencia.agregarElemento(2, 2);
+		// Eliminamos los elementos que no tengan simetricos
+		mAdyacencia.eliminarElemento(0, 0);
+		mAdyacencia.eliminarElemento(0, 1);
+		mAdyacencia.eliminarElemento(0, 2);
+		mAdyacencia.eliminarElemento(1, 1);
+		mAdyacencia.eliminarElemento(2, 2);
+		assertAll(
+				"Metodo eliminarElementoTest()",
+			() -> assertFalse(mAdyacencia.existeElemento(0,0)),
+			() -> assertFalse(mAdyacencia.existeElemento(0,1)),
+			() -> assertFalse(mAdyacencia.existeElemento(0,2)),
+			() -> assertFalse(mAdyacencia.existeElemento(1,1)),
+			() -> assertFalse(mAdyacencia.existeElemento(2,2))
+		);
 	}
 	
 	@Test
 	public void eliminarElementoSimetricoTest() {
-		mAdyacencia.eliminarElemento(0, 3);
-		assertFalse(mAdyacencia.existeElemento(0,3));
+		mAdyacencia.agregarElemento(1, 0);
+		mAdyacencia.agregarElemento(1, 2);
+		mAdyacencia.agregarElemento(2, 0);
+		mAdyacencia.agregarElemento(2, 1);
+		
+		mAdyacencia.eliminarElemento(1, 0);
+		mAdyacencia.eliminarElemento(1, 2);
+		mAdyacencia.eliminarElemento(2, 0);
+		mAdyacencia.eliminarElemento(2, 1);
+		
+		assertAll(
+				"Metodo eliminarElementoSimetricoTest()",
+			() -> assertFalse(mAdyacencia.existeElemento(1,0)),
+			() -> assertFalse(mAdyacencia.existeElemento(1,2)),
+			() -> assertFalse(mAdyacencia.existeElemento(2,0)),
+			() -> assertFalse(mAdyacencia.existeElemento(2,1))
+		);
 	}
 
 	@Test
